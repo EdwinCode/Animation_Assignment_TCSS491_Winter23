f9c1455b92fab690a09de761da044d1476fd559b
Edwin <solisbrunoedwin@gmail.com>
2023-01-22 21:06:02 -0800

    Animate walking right

---
+++ player.js
@@ -0,0 +1,20 @@
+class Player {
+	constructor(game) {
+		this.game = game;
+		this.animator = new Animator(ASSET_MANAGER.getAsset("./player.png"),
+			0, 204, 98, 88, 7, 0.2);
+
+		this.x = 10;
+		this.y = 10;
+		this.speed = 128;
+	};
+
+	update() {
+		this.x += this.speed * this.game.clockTick;
+		if(this.x > 1024) this.x = 0;
+	};
+
+	draw(ctx) {
+		this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y)
+	};
+}
